94b8726a8f595fb6378621f7b698c542df0b047e
yangss <cn.yangss@gmail.com>
2018-05-28 16:07:26 +0800

    提交Pr时，CI不过，提示Could not find .travis.yml, using standard configuration. 放入一个文件试验一下

--- demo/scripts/test.js
+++ demo/scripts/test.js
@@ -8,9 +8,9 @@ process.env.PUBLIC_URL = '';
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
-  throw err;
-});
+// process.on('unhandledRejection', err => {
+//   throw err;
+// });
 
 // Ensure environment variables are read.
 require('../config/env');
