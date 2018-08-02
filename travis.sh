721ee15ebeeacf9dfc2787581b2a4748c3d8c0f4
fanjinhao <fanfan@easemob.com>
2018-08-02 17:16:07 +0800

    travis配置调试

--- travis.sh
+++ travis.sh
@@ -5,26 +5,43 @@ packing(){
     cd ./demo
 	npm install
     cd ../
+    echo -e "\nINSTALL DONE.\n"
+
     cd ./webrtc
     npm link
     cd ../
-    cd ./demo
-    npm link easemob-webrtc
-    cd ../
+    echo -e "\nCREATE [WEBRTC] GLOBAL LINK DONE.\n"
+
     cd ./emedia
     npm link
     cd ../
+    echo -e "\nCREATE [EMEDIA] GLOBAL LINK DONE.\n"
+
+    cd ./demo
+    npm link easemob-webrtc
+    cd ../
+    echo -e "\nLINK [WEBRTC] DONE.\n"
+
     cd ./demo
     npm link easemob-emedia
     cd ../
+    echo -e "\nLINK [EMEDIA] DONE.\n"
+
     cd ./demo
     npm run build
+    cd ../
+    echo -e "\nBUILD DONE.\n"
 }
 
 upload(){
 	# 为了不修改 ci，copy 一份
+    echo -e "\nCOPY files...\n"
 	cp -r ./demo/build ./chatdemo-webim
+   
+    echo -e "\nZIP files...\n"
 	zip -r $TRAVIS_TAG.zip chatdemo-webim
+    
+    echo -e "\nUPLOAD ZIP...\n"
 	curl -v -F r=releases -F hasPom=false -F e=zip -F g=com.easemob.chatdemo2 -F a=webim -F v=$TRAVIS_TAG -F p=zip -F file=@$TRAVIS_TAG.zip -u ${nexus_auth} https://hk.nexus.op.easemob.com/nexus/service/local/artifact/maven/content
 }
 
