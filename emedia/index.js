5802d1c7fa89db93351134fd0c757a9ba89d9329
yangss <cn.yangss@gmail.com>
2018-08-22 15:23:38 +0800

    引入EMedia 2.0.1.9941cc5

--- emedia/index.js
+++ emedia/index.js
@@ -1,2 +1,3 @@
-var emedia = require('/Users/DATA/WORK.HOME/projects/CO./EASEMOB_2016.05.03~/EMedia/webrtc/dist/EMedia_sdk-dev');
+//var emedia = require('/Users/DATA/WORK.HOME/projects/CO./EASEMOB_2016.05.03~/EMedia/webrtc/dist/EMedia_sdk-dev');
+var emedia = require('./EMedia_sdk-2.0.1.9941cc5');
 module.exports = emedia;
