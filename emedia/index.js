876a417b70ee4d63b45dadb2bdb3749313b1837d
yangss <cn.yangss@gmail.com>
2018-09-07 16:21:53 +0800

    chrome 61版本存在与safari类似问题：混音发布时 video offer sendrecv，answer也是sendrecv，但是没有video数据，audio标签播放不了声音

--- emedia/index.js
+++ emedia/index.js
@@ -1,3 +1,3 @@
 //var emedia = require('/Users/DATA/WORK.HOME/projects/CO./EASEMOB_2016.05.03~/EMedia/webrtc/dist/EMedia_sdk-dev');
-var emedia = require('./EMedia_sdk-2.1.1.cc63e0c');
+var emedia = require('./EMedia_sdk-2.1.1.de86e59');
 module.exports = emedia;
