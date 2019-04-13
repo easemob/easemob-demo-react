a14c61689608c2a2aaf664aa7d7c3129f1efee63
zhangdong <973849793@qq.com>
2019-04-13 19:26:24 +0800

    群组操作通知

--- sdk/src/muc/HandleMucMessage.js
+++ sdk/src/muc/HandleMucMessage.js
@@ -18,23 +18,86 @@ var handleMessage = function(meta, status, conn){
 	var messageBodyMessage = self.context.root.lookup("easemob.pb.MUCBody");
     var thirdMessage = messageBodyMessage.decode(meta.payload);
     var msgId = new Long(meta.id.low, meta.id.high, meta.id.unsigned).toString();
-    var type = null;
-    // if (thirdMessage.type === 1) {             //messagetype 群组/聊天室。。。。
-    //     type = "chat";
-    // }
-    // else if (thirdMessage.type === 2) {
-    //     type = "groupchat";
-    // }
-    // else if (thirdMessage.type === 3) {
-    //     type = "chatroom";
-    // }
-    // else if(thirdMessage.type === 5){
-    //     type = "deliver_ack";
-    //     return;
-    // }
-    console.log(thirdMessage.operation);
-    
-    
+    var operation = thirdMessage.operation;
+
+    function condition(operation){
+        let info = {
+            type: '',
+            owner: thirdMessage.from.name,
+            gid: thirdMessage.mucId.name,
+            from: thirdMessage.from,
+            to: thirdMessage.to,
+            chatroom: thirdMessage.isChatroom,
+            status: thirdMessage.status
+        }
+
+        return({
+            24: () => {
+                info.type = 'removeMute';//解除禁言
+                conn.onPresence(info);
+            },
+            23: () => {
+                info.type = 'addMute';//禁言
+                conn.onPresence(info);
+            },
+            22: () => {
+                info.type = 'removeAdmin';//去除管理员
+                conn.onPresence(info);
+            },
+            21: () => {
+                info.type = 'addAdmin'; //成为管理员
+                conn.onPresence(info);
+            },
+            19: () => {
+                info.type = 'direct_joined'; //直接拉进群了
+                conn.onPresence(info);
+            },
+            18: () => {
+                info.type = thirdMessage.isChatroom ? 'leaveChatRoom' : 'absence';
+                //info.type = 'absence'; //退出群了
+                conn.onPresence(info);
+            },
+            17: () => {
+                info.type = thirdMessage.isChatroom ? 'joinChatRoomSuccess' : 'presence';
+                //info.type = 'presence'; //进群了
+                conn.onPresence(info);
+            },
+            13: () => {
+                info.type = 'allow'; //移除黑名单
+                info.reason = thirdMessage.reason;
+                conn.onPresence(info);
+            },
+            10: () => {
+                info.type = 'removedFromGroup'; //被移出群 或者被加入黑名单
+                conn.onPresence(info);
+            },
+            6:  () => {
+                info.type = 'joinPublicGroupDeclined'; //加群被拒绝
+                conn.onPresence(info);
+            },
+            5: () => {
+                info.type = 'joinPublicGroupSuccess'; //加群成功
+                conn.onPresence(info);
+            },
+            4: () => {
+                info.type = 'joinGroupNotifications'; //申请加群
+                info.reason = thirdMessage.reason;
+                conn.onPresence(info);
+            },
+            1: () => {
+                info.type = 'deleteGroupChat'; //群组解散
+                conn.onPresence(info);
+            },
+            
+        }[operation] || function(){
+            console.log(`%c没有匹配${operation}类型`, 'background: green')
+        })()
+    }
+
+    condition(operation)
+
+    console.log('%c'+'群组消息', 'background: green')
+    console.log(thirdMessage);
 }
 
 export {handleMessage}
