64659b25d735971d2fc7c742ef65c749449f046e
zhangdong <973849793@qq.com>
2019-06-11 16:18:32 +0800

    增加扩展消息

--- demo/src/redux/WebIMRedux.js
+++ demo/src/redux/WebIMRedux.js
@@ -278,11 +278,12 @@ WebIM.conn.listen({
             store.dispatch(RosterActions.topRoster(from))
             //新的会议要求消息，使用text message实现
             if(WebIM && WebIM.call && message && message.ext && message.ext[0].key === "conferenceId"){
+                var msgExtension = message.ext[1]&&message.ext[1].key === "msg_extension"&&JSON.parse(message.ext[1].stringValue)
                 var options = {
                     confrId: message.ext[0].stringValue,
-                    password: message.ext.password || "",
-                    gid: message.ext.msg_extension && message.ext.msg_extension.group_id,
-                    inviter: message.from
+                    password: (message.ext[2].key === "password"&&message.ext[2].stringValue) || "",
+                    gid: msgExtension.group_id,
+                    inviter: msgExtension.inviter
                 }
                 WebIM.call.listener.onInvite(from, options)
             }
