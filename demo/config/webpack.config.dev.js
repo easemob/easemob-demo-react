26148019d507b375bf14249981720bd588223c2e
zhangdong <973849793@qq.com>
2019-07-02 15:12:16 +0800

    解决起服务时有警告

--- demo/config/webpack.config.dev.js
+++ demo/config/webpack.config.dev.js
@@ -110,6 +110,7 @@ module.exports = {
     module: {
         unknownContextCritical : false,
         strictExportPresence: true,
+        exprContextCritical: false,
         rules: [
             // TODO: Disable require.ensure as it's not a standard language feature.
             // We are waiting for https://github.com/facebookincubator/create-react-app/issues/2176.
