e4f587b2d569d61bcf60bbf75bcfcab2a6ed7470
zhangdong <973849793@qq.com>
2019-07-03 12:30:22 +0800

    删除image文件夹

--- image/drone/build_docker_online.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd webim-h5-online
-
-docker images |grep webim-h5-online|awk '{print $3}'|xargs docker rmi -f
-
-docker build -t docker-registry-cn.easemob.com/kubernetes/im/webim-h5-online:latest .
-
-docker push docker-registry-cn.easemob.com/kubernetes/im/webim-h5-online:latest
