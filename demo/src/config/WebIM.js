a03e85a089b722639bdfb4560452e649fb434f00
suqingxia <suqingxia@picooc.com>
2020-05-15 18:06:14 +0800

    update easemob-emedia

--- demo/src/config/WebIM.js
+++ demo/src/config/WebIM.js
@@ -2,10 +2,10 @@
 //import "script-loader!easemob-websdk/dist/strophe-1.2.8.js"
 /* eslint-enable */
 import websdk from './webimSDK3.1.1'
-import webrtc from './EMedia_x1v1'
+// import webrtc from './EMedia_x1v1'
 
 // import websdk from 'easemob-websdk'
-// import webrtc from 'easemob-webrtc'
+import webrtc from 'easemob-webrtc'
 
 import config from 'WebIMConfig'
 import emoji from './emoji'
