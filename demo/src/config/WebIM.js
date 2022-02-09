4021de6c326b8c1146aa8ae96d6a2832818ab99f
myZhangDong <973849793@qq.com>
2022-02-09 15:38:45 +0800

    update IM SDK 4.0.3

--- demo/src/config/WebIM.js
+++ demo/src/config/WebIM.js
@@ -4,9 +4,9 @@
 
 // add dataReport sdk
 // import websdk from './Easemob-chat-3.6.3'
-import websdk from './Easemob-chat'
+// import websdk from './Easemob-chat'
 
-// import websdk from 'easemob-websdk'
+import websdk from 'easemob-websdk'
 // import webrtc from 'easemob-webrtc'
 // import emedia from './EMedia_sdk-dev'
 import config from 'WebIMConfig'
