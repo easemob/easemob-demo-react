a9b3842809e677c0596501b224820760ed2b170f
stwang <464905943@qq.com>
2022-02-10 12:09:59 +0800

    update im sdk 4.0.4

--- demo/src/config/WebIM.js
+++ demo/src/config/WebIM.js
@@ -6,7 +6,7 @@
 // import websdk from './Easemob-chat-3.6.3'
 // import websdk from './Easemob-chat'
 
-import websdk from 'easemob-websdk'
+import websdk from './Easemob-chat-4.0.4'
 // import webrtc from 'easemob-webrtc'
 // import emedia from './EMedia_sdk-dev'
 import config from 'WebIMConfig'
