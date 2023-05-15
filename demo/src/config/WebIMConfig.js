489f998ba60302ffe7d75451375ad2fcb1213213
stwang <464905943@qq.com>
2023-05-15 11:57:11 +0800

    fix: update deviceId

--- demo/src/config/WebIMConfig.js
+++ demo/src/config/WebIMConfig.js
@@ -116,7 +116,7 @@ var config = {
    */
     enableLocalStorage: true,
 
-    deviceId: 'webim',
+    deviceId: `${new Date().getTime()}`,
 
     AgoraAppId: '15cb0d28b87b425ea613fc46f7c9f974' // 需要替换成自己的声网 appId，此 appId 有限量，仅供参考使用，同时获取声网 token 的接口仅能供此 appId 使用，换成自己的 appId 后需要自己去实现 app server 获取声网token。
 }
