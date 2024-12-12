487d4572d51ad1cb15d1ca35c9a93838c86dae92
myZhangDong <973849793@qq.com>
2024-12-12 18:23:17 +0800

    feat: update config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const appKey = "your appKey"; // 环信AppKey，IM功能使用
+export const appKey = "orgName#appName"; // 环信AppKey，IM功能使用
 export const APP_ID = "your appId"; // 声网AppId，音视频功能使用
 export const UIKIT_VERSION = "1.4.0";
 export const SDK_VERSION = "4.11.0";
